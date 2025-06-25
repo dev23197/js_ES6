f3f63a97340bf608519346a89f70928627a4cace
Dev <dev1672004@gmail.com>
2025-06-25 16:09:26 +0530

    Add Number properties example like EPSILON, MIN_SAFE_INTEGER, MAX_SAFE_INTEGER

---
+++ 18-numberproperties.js
@@ -0,0 +1,12 @@
+// console.log(Number.EPSILON); 
+
+
+// console.log(Number.MIN_SAFE_INTEGER); 
+
+
+// console.log(Number.MAX_SAFE_INTEGER);
+
+let x = Number;
+Number.isInteger(10);     
+Number.isInteger(10.5); 
+console.log(Number);
