dbabaebc6768dcd2999505712df1eb851a00d41c
Dev <dev1672004@gmail.com>
2025-06-25 16:04:21 +0530

    Add default parameter values example in functions

---
+++ 13-defaultparametervalues.js
@@ -0,0 +1,13 @@
+function myFunction(x, y = 10) {
+
+  return x + y;
+}
+console.log(myFunction(5));
+
+
+// function myFunction(){
+//  x = 10;
+//  y = 14;
+//   return x + y;
+// }
+// console.log(myFunction());
